7cae3477be435369ec45ac77a2c0591f7e404556
irenelopez42 <irenelopez_1996@hotmail.com>
2017-03-16 20:49:50 +0000

    small changes

--- GUI_lastversion.py
+++ GUI_lastversion.py
@@ -738,8 +738,9 @@ def check_queue():
             run.grid(row=20)
         if task == 6:
             global listbuttons
-            for i in range(0,len(listbuttons)):
-                listbuttons[i].grid_forget()
+            if len(listbuttons) > 0:
+                for i in range(0,len(listbuttons)):
+                    listbuttons[i].grid_forget()
         if task == 7:
             drawingp.grid_forget()
             plotb.grid(row=20, sticky=E)
@@ -764,7 +765,6 @@ def abort():
     for process in pool:
         process.terminate()
         process.join()
-
     with queue.mutex:
         queue.queue.clear()
         
@@ -942,7 +942,6 @@ def run_analysis():
         selection.append(missE_chk) 
         
     processingDict = CustomConfiguration.Processes
-    print CustomConfiguration.Job["Fraction"]
         
     CustomConfiguration.Job["Batch"] = True
     jobs = [NewJob.NewJob(processName,CustomConfiguration.Job,
@@ -959,13 +958,9 @@ def run_analysis():
                 
     pool.reverse()
     for process in pool:
-        print "test2"
-        if makeplots:
-            process.join()
-            task = 1
-            queue.put(task)
-    
-    print "test3"
+        process.join()
+        task = 1
+        queue.put(task)
    
     task = 3
     queue.put(task)
@@ -988,11 +983,8 @@ def run_analysis():
     del listphotosbig[:]
     global listcommands
     del listcommands[:]
-    global listbuttons
-    if len(listbuttons) > 0:
-        task = 6
-        queue.put(task)
-    del listbuttons[:]
+    task = 6
+    queue.put(task)
     global listlabels
     del listlabels[:]
     previousplots=glob.glob('Output/*.gif')
